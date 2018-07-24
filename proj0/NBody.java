fccf2aa4e00c681966c5e66f0200b595f946d2a6
hyde1985 <hyde_sun@sina.com>
2018-07-24 14:51:26 +0800

    Finish proj0 - Writing the NBody Class - readPlanets

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -10,5 +10,23 @@ public class NBody {
 		   double radius = in.readDouble();
 		   return radius;
 	   }
+
+	   public static Planet[] readPlanets(String fileName) {
+
+	   		In in = new In(fileName);
+	   		int numOfPlanets = in.readInt();
+	   		Planet[] planets = new Planet[numOfPlanets];
+	   		in.readDouble();
+	   		for(int i = 0; i < planets.length; i++) {
+	   			double xxPos = in.readDouble();
+	   			double yyPos = in.readDouble();
+	   			double xxVel = in.readDouble();
+	   			double yyVel = in.readDouble();
+	   			double mass = in.readDouble();
+	   			String imgFileName = in.readString();
+	   			planets[i] = new Planet(xxPos, yyPos, xxVel, yyVel, mass, imgFileName);
+	   		}
+	   		return planets;
+	   }
 	
 }
